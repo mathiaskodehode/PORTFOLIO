de2b3680b83c5dfa94a940121d7fd11af0206b84
Mathias <4mathias.kodehode@gmail.com>
2026-09-10 10:59:19 +0200

    switch to file-based vue router and remove manual router configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import VueRouter from "vue-router/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
+    plugins: [VueRouter(), vue(), vueDevTools()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
