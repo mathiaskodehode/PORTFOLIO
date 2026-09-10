0aa00090fb570840440d97bb8fedbe699cbcc568
Mathias <4mathias.kodehode@gmail.com>
2026-09-10 11:23:18 +0200

    add hot module reload handling for vue router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
-import { routes } from "vue-router/auto-routes";
+import { routes, handleHotUpdate } from "vue-router/auto-routes";
 import App from "./App.vue";
 import "./style.css";
 
@@ -8,5 +8,7 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+if (import.meta.hot) handleHotUpdate(router);
 
 createApp(App).use(router).mount("#app");
