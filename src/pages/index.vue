6dd3d6f9c07316963a3fec23a5b21fa4c063918b
Mathias <4mathias.kodehode@gmail.com>
2026-09-22 09:00:14 +0200

    restructure homepage sections and add about me content

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,23 +6,19 @@ const logoPath = (name: string) => `${import.meta.env.BASE_URL}images/logos/${na
 </script>
 
 <template>
-    <section class="hero">
+    <section class="hero homeSection">
         <p class="eyebrow">PORTFOLIO</p>
         <h1>Mathias Hareide</h1>
         <p>developing developing developing developing developing</p>
+        <h2>About me</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iusto consequatur illum placeat nisi reprehenderit dignissimos et saepe ab impedit tenetur deleniti repudiandae sed veritatis eligendi natus, doloribus
+            esse eaque, perspiciatis, debitis sit quibusdam maiores corrupti soluta? Facilis numquam adipisci atque, aut a minima distinctio doloribus, nihil ab odio explicabo aspernatur tenetur nisi voluptates id asperiores
+            architecto eius alias nam! Delectus, iure fuga ratione deserunt necessitatibus recusandae maiores dignissimos!
+        </p>
     </section>
 
-    <section>
-        <h2>Technologies</h2>
-        <div class="technologiesSection">
-            <div class="tecnologyItem" v-for="(e, i) in ['C', 'C Sharp', 'Javascript', 'Typescript', 'Git', 'Github', 'React', 'Vue.js', 'Unity', 'Raylib', 'Figma', 'Node.js']" :key="i">
-                <h3>{{ e }}</h3>
-                <img :src="logoPath(e)" :alt="`${e} logo`" class="technologyLogo" />
-            </div>
-        </div>
-    </section>
-
-    <section class="home-section">
+    <section class="home-projects-section homeSection">
         <div class="section-heading">
             <h2>Projects</h2>
             <RouterLink to="/projects">View all</RouterLink>
@@ -38,6 +34,16 @@ const logoPath = (name: string) => `${import.meta.env.BASE_URL}images/logos/${na
                 </div>
                 <div class="project-list__arrow" aria-hidden="true"></div>
             </RouterLink>
+        </div>
+    </section>
+
+    <section class="technologiesSection homeSection">
+        <h2>Technologies</h2>
+        <div class="technologiesGrid">
+            <div class="tecnologyItem" v-for="(e, i) in ['C', 'C Sharp', 'Javascript', 'Typescript', 'Git', 'Github', 'React', 'Vue.js', 'Unity', 'Raylib', 'Figma', 'Node.js']" :key="i">
+                <h3>{{ e }}</h3>
+                <img :src="logoPath(e)" :alt="`${e} logo`" class="technologyLogo" />
+            </div>
         </div>
     </section>
 </template>
